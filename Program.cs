4a7c7fabae446a9bd0fc40be6cbc0a6cdf6f3604
Malay Doshi <malaypdoshi@hotmail.com>
2020-04-29 18:53:30 +0530

    Alert This May Create a new Branch Remotely for Programmer A

--- Program.cs
+++ Program.cs
@@ -9,7 +9,14 @@ namespace GitWithTeamsExplorerVS2019
             Console.WriteLine("Hello World!");
             //First Edit By Me
             //Edit Made By Me Only But on GitHub.com
+
+            //This is First Branch Assigned to Programmer A 
+            //He will Make A Function Which will do nothing 
+            //then we will try to merge it in local & then remote
             Console.ReadKey();
+        }
+        public void DoNothing() {
+            Console.WriteLine("I am Doing Nothing, I Am Programmer A, I have Done My Task of Creating this Function");
         }
     }
 }
